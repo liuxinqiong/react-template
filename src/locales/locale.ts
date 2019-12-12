99149d3573cffb29910e19a759c43108c8459295
liuxinqiong <lxq@xkool.info>
2019-12-12 10:59:33 +0800

    add auto fotmat

--- src/locales/locale.ts
+++ src/locales/locale.ts
@@ -1,48 +1,48 @@
-import { Context } from 'react'
+import { Context } from "react";
 
-const localeKey = 'locale'
-const defaultLocaleCode = 'zh-CN'
+const localeKey = "locale";
+const defaultLocaleCode = "zh-CN";
 
-let localeContext: any
+let localeContext: any;
 
 function setLocale(lang: string, realReload = true) {
-  const localeExp = new RegExp(`^([a-z]{2})-?([A-Z]{2})?$`)
+  const localeExp = new RegExp(`^([a-z]{2})-?([A-Z]{2})?$`);
   if (lang !== undefined && !localeExp.test(lang)) {
     // for reset when lang === undefined
-    throw new Error('setLocale lang format error')
+    throw new Error("setLocale lang format error");
   }
   if (getLocale() !== lang) {
-    window.localStorage.setItem(localeKey, lang || '')
+    window.localStorage.setItem(localeKey, lang || "");
     if (localeContext && !realReload) {
-      localeContext.reloadAppLocale()
+      localeContext.reloadAppLocale();
     }
     if (realReload) {
-      window.location.reload()
+      window.location.reload();
     }
     if (window.dispatchEvent) {
-      const event = new Event('languagechange')
-      window.dispatchEvent(event)
+      const event = new Event("languagechange");
+      window.dispatchEvent(event);
     }
   }
 }
 
 // 优先级：路径参数 > localStorage > navigator > default
 function getLocale() {
-  const queryLang = new URL(location.href).searchParams.get(localeKey)
+  const queryLang = new URL(window.location.href).searchParams.get(localeKey);
   const localLang =
-    typeof localStorage !== 'undefined'
+    typeof localStorage !== "undefined"
       ? window.localStorage.getItem(localeKey)
-      : ''
+      : "";
   const isNavigatorLanguageValid =
-    typeof navigator !== 'undefined' && typeof navigator.language === 'string'
-  const browserLang = isNavigatorLanguageValid ? navigator.language : ''
-  return queryLang || localLang || browserLang || defaultLocaleCode
+    typeof navigator !== "undefined" && typeof navigator.language === "string";
+  const browserLang = isNavigatorLanguageValid ? navigator.language : "";
+  return queryLang || localLang || browserLang || defaultLocaleCode;
 }
 
 function setLocaleContext(
   context: Context<{ locale: string; reloadAppLocale: Function }>
 ) {
-  localeContext = context
+  localeContext = context;
 }
 
-export { setLocale, getLocale, setLocaleContext }
+export { setLocale, getLocale, setLocaleContext };
